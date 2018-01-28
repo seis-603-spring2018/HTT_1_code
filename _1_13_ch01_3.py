560f7d308723a99b7a5313da4425d135a3067f06
seis-603-spring2018 <seis.603.spring2018@gmail.com>
2018-01-27 19:52:29 -0600

    first commit

---
+++ _1_13_ch01_3.py
@@ -0,0 +1,13 @@
+#
+# HTT Ch 1 code example:
+#
+# Section 1.13: ch01_3
+#
+
+#---------------------------------------------------
+# This demo program shows off how elegant Python is!
+# Written by Joe Soap, December 2010.
+# Anyone may freely copy or modify this program.
+#---------------------------------------------------
+
+print("Hello, World!")     # Isn't this easy!
---
+++ _1_4_ch01_1.py
@@ -0,0 +1,7 @@
+#
+# HTT Ch 1 code example:
+#
+# Section 1.12: ch01_2
+#
+
+print("Hello, World!")
